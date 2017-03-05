9a632829baf989d6aa40b8a77c626ca9357c872c
Zieger Jheng <shjheng@gmail.com>
2017-03-05 15:26:46 +0800

    first workable version

---
+++ routes/web.php
@@ -0,0 +1,125 @@
+<?php
+
+use Facebook\Facebook;
+use Facebook\PersistentData\PersistentDataInterface;
+
+class MyLaravelPersistentDataHandler implements PersistentDataInterface
+{
+    protected $sessionPrefix = 'FBRLH_';
+
+    public function get($key)
+    {
+        return session($key);
+    }
+
+    public function set($key, $value)
+    {
+        session([$key => $value]);
+    }
+}
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| This file is where you may define all of the routes that are handled
+| by your application. Just tell Laravel the URIs it should respond
+| to using a Closure or controller method. Build something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/slides', function () {
+    return view('slides');
+});
+
+Route::get('/3rd/facebook/login', function () {
+    $fb = new Facebook([
+        'app_id' => config('facebook.app_id'),
+        'app_secret' => config('facebook.app_secret'),
+        'persistent_data_handler' => new MyLaravelPersistentDataHandler()
+    ]);
+
+    $helper = $fb->getRedirectLoginHelper();
+    $permissions = ['user_managed_groups'];
+    $loginUrl = $helper->getLoginUrl(config('app.url') . '/3rd/facebook/login-callback', $permissions);
+
+    return Redirect::away($loginUrl);
+});
+
+Route::get('/3rd/facebook/login-callback', function () {
+    $fb = new Facebook([
+        'app_id' => config('facebook.app_id'),
+        'app_secret' => config('facebook.app_secret'),
+        'persistent_data_handler' => new MyLaravelPersistentDataHandler()
+    ]);
+
+    $helper = $fb->getRedirectLoginHelper();
+
+    try {
+        $accessToken = $helper->getAccessToken();
+    } catch(Facebook\Exceptions\FacebookResponseException $e) {
+        dd($e->getMessage());
+    } catch(Facebook\Exceptions\FacebookSDKException $e) {
+        dd($e->getMessage());
+    }
+
+    if(false == $accessToken->isLongLived()) {
+        $oAuth2Client = $fb->getOAuth2Client();
+        $accessToken = $oAuth2Client->getLongLivedAccessToken($accessToken);
+    }
+
+    session(['fb_user_access_token' => (string) $accessToken]);
+
+    return redirect()->route('fbGroupPhoto');
+});
+
+Route::get('/photoFeeds', function() {
+    $fbGroupPhotos = getPhotoFeeds();
+    return response()->json([ 'feeds' => $fbGroupPhotos ]);
+})->name('fbGroupPhoto');
+
+function getPhotoFeeds()
+{
+    $fb = new Facebook([
+        'app_id' => config('facebook.app_id'),
+        'app_secret' => config('facebook.app_secret'),
+        'persistent_data_handler' => new MyLaravelPersistentDataHandler()
+    ]);
+
+    $fb->setDefaultAccessToken(session('fb_user_access_token'));
+
+    $response = $fb->get('/885989284866779/feed?fields=from,message,object_id,type');
+    $groupFeeds = $response->getDecodedBody();
+    
+    $fbGroupPhotos = [];
+    while(count($groupFeeds['data']) > 0) {
+        foreach($groupFeeds['data'] as $feed ){
+            if('photo' != $feed['type']){
+                continue;
+            }
+
+            $response = $fb->get($feed['object_id'] . '?fields=images');
+            $imageData = $response->getDecodedBody();
+
+            $fbGroupPhotos[] = [
+                'id' => $feed['object_id'],
+                'userName' => $feed['from']['name'],
+                'message' => (array_key_exists('message', $feed)) ? $feed['message'] : '',
+                'photoUrl' => $imageData['images'][0]['source']
+            ];
+        }
+
+        $next = parse_url($groupFeeds['paging']['next']);
+        $nextQuery = '/885989284866779/feed?' . $next['query'];
+
+        $response = $fb->get($nextQuery);
+        $groupFeeds = $response->getDecodedBody();
+    }
+
+    return $fbGroupPhotos;
+}
